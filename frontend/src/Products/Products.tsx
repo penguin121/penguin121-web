1efd822c336eeb5b9cd5e5529ef4791d642d4604
Tom Kennedy <holtsrule1@gmail.com>
2023-03-24 21:24:30 -0400

    Improving catalog feature

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,27 +1,7 @@
-import data from "./data";
-
 function Products() {
     return (
-        <div className="content">
-            <ul className ="products">
-                {data.products.map((product) => (
-                    <li>
-                        <div className="product">
-                            <img
-                                className="product-image"
-                                src={product.imageUrl}
-                                alt="product"
-                            />
-                            <div className="product-name">
-                                <a href="product.html">{product.name}</a>
-                            </div>
-                            <div className="product-brand">{product.brand}</div>
-                            <div className="product-price">{product.price}</div>
-                            <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} reviews)</div>
-                        </div>
-                    </li>
-                ))};
-            </ul>
+        <div>
+            <h2>Products</h2>
         </div>
     );
 }
